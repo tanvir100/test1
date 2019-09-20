c32f0aea9ee17e14836cc9faa06066459be1c723
test <>
2019-09-19 20:20:58 -0400

    2nd Commit

---
+++ src/Test.java
@@ -0,0 +1,30 @@
+
+
+/**
+ *
+ * @author shuvr
+ */
+public class Test {
+
+    private String name;
+
+    /**
+     * Get the value of name
+     *
+     * @return the value of name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param name new value of name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
